392ad62d56253b047d7ad5c5bc7507f6ecada522
Adrien Pavao <adrien.pavao@gmail.com>
2019-07-11 02:49:57 +0200

    Correct typo.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("autopandas/README.md", "r") as fh:
 
 setup(
      name='autopandas',
-     version='0.1.8,
+     version='0.1.8',
      author="Adrien Pavao",
      author_email="adrien.pavao@gmail.com",
      description="Process, visualize and use data easily.",
