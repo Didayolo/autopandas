3aa0b6cefaadcf6f922743191a1001bcb6bbf5ec
Adrien Pavao <adrien.pavao@gmail.com>
2019-10-24 01:48:21 +0200

    Better import for generators module.

---
+++ autopandas/generators/__init__.py
@@ -0,0 +1,15 @@
+# Generators module
+
+# Import all generators
+from .artificial import Artificial
+from .copycat import Copycat
+from .anm import ANM
+from .copula import Copula
+from .gmm import GMM
+from .kde import KDE
+from .autoencoder import AE
+from .vae import VAE
+from .fae import FAE
+# TODO
+# GAN, WGAN, medGAN
+# SAM
